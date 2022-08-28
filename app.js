f70753715fda3392cea3541ddb564243e44b0f7a
singla29nikhil <98166804+singla29nikhil@users.noreply.github.com>
2022-08-29 01:36:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -122,11 +122,6 @@ app.post("/delete", function(req, res){
 
 
 });
-
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started");
 });
